adcbba795934d27aa758c12bd74b23318d0f8a39
Pavan MV <venkatapavan.muppala@jci.com>
2025-02-28 20:09:39 +0530

    working lora adapter

--- practice/sft_finetune.py
+++ practice/sft_finetune.py
@@ -1,75 +1,104 @@
-# Import necessary libraries
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from datasets import load_dataset
-from trl import SFTConfig, SFTTrainer, setup_chat_format
+# # Import necessary libraries
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+# from datasets import load_dataset
+# from trl import SFTConfig, SFTTrainer, setup_chat_format
+# import torch
+
+# device = (
+#     "cuda"
+#     if torch.cuda.is_available()
+#     else "mps" if torch.backends.mps.is_available() else "cpu"
+# )
+
+# # Load the model and tokenizer
+# model_name = "HuggingFaceTB/SmolLM2-135M"
+# model = AutoModelForCausalLM.from_pretrained(
+#     pretrained_model_name_or_path=model_name
+# ).to(device)
+# tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
+# # Set up the chat format
+# model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
+
+# # Set our name for the finetune to be saved &/ uploaded to
+# finetune_name = "SmolLM2-FT-MVP"
+# finetune_tags = ["smol-course", "module_1"]
+# ds = load_dataset(path="HuggingFaceTB/smoltalk", name="everyday-conversations")
+
+# # Configure the SFTTrainer
+# sft_config = SFTConfig(
+#     dataset_text_field="messages",
+#     output_dir="./sft_output",
+#     max_steps=1000,  # Adjust based on dataset size and desired training duration
+#     per_device_train_batch_size=4,  # Set according to your GPU memory capacity
+#     learning_rate=5e-5,  # Common starting point for fine-tuning
+#     logging_steps=10,  # Frequency of logging training metrics
+#     save_steps=100,  # Frequency of saving model checkpoints
+#     evaluation_strategy="steps",  # Evaluate the model at regular intervals
+#     eval_steps=50,  # Frequency of evaluation
+#     use_mps_device=(
+#         True if device == "mps" else False
+#     ),  # Use MPS for mixed precision training
+#     hub_model_id=finetune_name,  # Set a unique name for your model
+# )
+
+# # Initialize the SFTTrainer
+# trainer = SFTTrainer(
+#     model=model,
+#     args=sft_config,
+#     train_dataset=ds["train"],
+#     tokenizer=tokenizer,
+#     eval_dataset=ds["test"],
+# )
+# trainer.train()
+# print("here")
+# from transformers import ViTForImageClassification
+
+# from trl import ORPOConfig, ORPOTrainer
+
+# # Configure ORPO training
+# orpo_config = ORPOConfig(
+#     learning_rate=1e-5,
+#     per_device_train_batch_size=4,
+#     gradient_accumulation_steps=4,
+#     max_steps=1000,
+#     orpo_alpha=1.0,  # Controls strength of preference optimization
+#     orpo_beta=0.1,   # Temperature parameter for odds ratio
+# )
+
+# # Initialize trainer
+# trainer = ORPOTrainer(
+#     model=model,
+#     args=orpo_config,
+#     train_dataset=dataset,
+#     tokenizer=tokenizer,
+# )
+# trainer.train()
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available() else "cpu"
-)
 
-# Load the model and tokenizer
-model_name = "HuggingFaceTB/SmolLM2-135M"
+import torch
+import transformers
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+peft_model_id = "ybelkada/opt-6.7b-lora"
+config = PeftConfig.from_pretrained(peft_model_id)
 model = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path=model_name
-).to(device)
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
-# Set up the chat format
-model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
-
-# Set our name for the finetune to be saved &/ uploaded to
-finetune_name = "SmolLM2-FT-MVP"
-finetune_tags = ["smol-course", "module_1"]
-ds = load_dataset(path="HuggingFaceTB/smoltalk", name="everyday-conversations")
-
-# Configure the SFTTrainer
-sft_config = SFTConfig(
-    dataset_text_field="messages",
-    output_dir="./sft_output",
-    max_steps=1000,  # Adjust based on dataset size and desired training duration
-    per_device_train_batch_size=4,  # Set according to your GPU memory capacity
-    learning_rate=5e-5,  # Common starting point for fine-tuning
-    logging_steps=10,  # Frequency of logging training metrics
-    save_steps=100,  # Frequency of saving model checkpoints
-    evaluation_strategy="steps",  # Evaluate the model at regular intervals
-    eval_steps=50,  # Frequency of evaluation
-    use_mps_device=(
-        True if device == "mps" else False
-    ),  # Use MPS for mixed precision training
-    hub_model_id=finetune_name,  # Set a unique name for your model
+    config.base_model_name_or_path,
+    return_dict=True,
+    load_in_8bit=True,
+    device_map="auto",
 )
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
-# Initialize the SFTTrainer
-trainer = SFTTrainer(
-    model=model,
-    args=sft_config,
-    train_dataset=ds["train"],
-    tokenizer=tokenizer,
-    eval_dataset=ds["test"],
-)
-trainer.train()
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
 print("here")
-from transformers import ViTForImageClassification
-
-from trl import ORPOConfig, ORPOTrainer
-
-# Configure ORPO training
-orpo_config = ORPOConfig(
-    learning_rate=1e-5,
-    per_device_train_batch_size=4,
-    gradient_accumulation_steps=4,
-    max_steps=1000,
-    orpo_alpha=1.0,  # Controls strength of preference optimization
-    orpo_beta=0.1,   # Temperature parameter for odds ratio
-)
-
-# Initialize trainer
-trainer = ORPOTrainer(
-    model=model,
-    args=orpo_config,
-    train_dataset=dataset,
-    tokenizer=tokenizer,
-)
-trainer.train()
